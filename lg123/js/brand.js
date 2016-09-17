a651a9af04560cd79824612ce61056dc2b5a580b
Mrzeron <343507100@qq.com>
2016-09-17 21:31:05 +0800

    2016-9-17

--- lg123/js/brand.js
+++
@@ -1,8 +0,0 @@
-$.aniAuto();
-$("#trbtn").hover(function () {
-    $(".logo").find(".img2").addClass("fadeInLeft").removeClass("fadeOutLeft");
-    $(".logo").find(".img1").addClass("fadeOutRight").removeClass("fadeInRight");
-},function(){
-    $(".logo").find(".img2").addClass("fadeOutLeft").removeClass("fadeInLeft");
-    $(".logo").find(".img1").addClass("fadeInRight").removeClass("fadeOutRight");
-});
